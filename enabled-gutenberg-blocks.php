827bbcc49bda9fd77abcf97aa28168b334e96ab9
juanmacivico87 <juanmacivico87@gmail.com>
2020-03-26 12:47:10 +0100

    Add description to «Enabled Gutenberg Blocks» snippet

--- enabled-gutenberg-blocks.php
+++ enabled-gutenberg-blocks.php
@@ -1,11 +1,14 @@
 <?php
-
+/**
+ * This function allows you to set blocks for frontpage (or any other you want)
+ */
 function enabled_homepage_gutenberg_blocks( $allowed_block_types )
 {
 	global $post;
     //If the page is set as "homepage", I add an array with all the blocks I want to load.
     //If not, it does not enter the conditional and the variable still contains the default Gutenberg blocks.
-    if ( $post->ID === intval( get_option( 'page_on_front' ) ) ) {
+    if ( $post->ID === intval( get_option( 'page_on_front' ) ) )
+    {
         $allowed_block_types = array(
             'core/paragraph'
         );
